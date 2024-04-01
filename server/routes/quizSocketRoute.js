c8fe913bd75a78635ef6eb6b937b3065c886ef18
DiagoEnder <thenubic@gmail.com>
2024-04-01 15:55:57 +0700

    test 2 server

---
+++ server/routes/quizSocketRoute.js
@@ -0,0 +1,14 @@
+// const express = require("express");
+// const http = require("http");
+// const socketIo = require("socket.io");
+// const cors = require("cors");
+// const app = express();
+
+// const server = http.createServer(app);
+// app.use(cors());
+// const io = socketIo(server, {
+//    cors: {
+//      origin: "http://localhost:8173",
+//      methods: ["GET", "POST"],
+//    },
+// });
